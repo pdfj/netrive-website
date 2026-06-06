431d2313b660b492fd402bc690e6e7e5311be321
pdfj <malgasuser@gmail.com>
2026-06-06 13:57:27 +0200

    Add status 303 to redirect after sign out

--- app/api/auth/signout/route.ts
+++ app/api/auth/signout/route.ts
@@ -4,5 +4,8 @@ import { createClient } from "@/lib/supabase/server";
 export async function POST() {
   const supabase = createClient();
   await supabase.auth.signOut();
-  return NextResponse.redirect(new URL("/auth/login", process.env.NEXT_PUBLIC_SITE_URL ?? "https://www.netrive.com"));
+  return NextResponse.redirect(
+    new URL("/auth/login", process.env.NEXT_PUBLIC_SITE_URL ?? "https://www.netrive.com"),
+    { status: 303 },
+  );
 }
