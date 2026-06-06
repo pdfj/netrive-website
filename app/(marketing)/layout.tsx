72a5efae2e34c4493e0d6a24e6e0dddc79a3f43e
pdfj <malgasuser@gmail.com>
2026-06-06 16:10:49 +0200

    Add AiChat component to MarketingLayout

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,6 +1,7 @@
 import { Navbar } from "@/components/layout/Navbar";
 import { Footer } from "@/components/layout/Footer";
 import { WhatsAppButton } from "@/components/layout/WhatsAppButton";
+import { AiChat } from "@/components/AiChat";
 
 export default function MarketingLayout({
   children,
@@ -13,6 +14,7 @@ export default function MarketingLayout({
       {children}
       <Footer />
       <WhatsAppButton />
+      <AiChat />
     </>
   );
 }
